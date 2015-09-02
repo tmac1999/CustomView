42fda9f41600717e8bfc6bd9925576e7aae7a151
mrz <119141701@qq.com>
2015-09-02 23:55:31 +0800

    test.txt

---
+++ CustomDragLayout/src/com/example/myDragLayout/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myDragLayout;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        String typeName = info.getTypeName();
+        Toast.makeText(this, "当前网络类型="+typeName, 0).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
